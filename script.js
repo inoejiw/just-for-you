a8e1d226cacf0e2a067e828ff791ee92a2e7f25f
Ihsan Nursyahjiwa <ihsan.nursyahjiwa1@gmail.com>
2025-04-12 17:48:36 +0700

    gabut

---
+++ script.js
@@ -0,0 +1,44 @@
+var nama = prompt('Boleh kasih tau nggk nama panggilannya apa... :')
+
+switch (nama) {
+    case 'Huda' :
+        alert('Halooo ' + nama);
+
+        var naleng = prompt('Masukin nama lengkapnya ' + nama + ' yahhh...')
+
+        switch (naleng) {
+            case 'Siti Huda Muzdalifah':
+                alert('Haii ' + nama + ' Kamu tau nggk, aku tuh bikin sama kirim website ini cuman buat kamuu loh... hhe');
+                alert('jadi lewat program yang aku buat ini ada yang sebenernya pengen aku sampein, bahkan dari dulu sihh pengennya hhe. Tapi yahh karena ada beberapa pertimbangan jadi aku baru pengen bilang sekarang...')
+                alert('Ga kerasa yahh kita kuliah sekarang udah tingkat 3 hhe, yang mana cuman setaun lagi maybe kita udah nggk kuliah yahh...')
+                alert('nah sebelum memang nanti nggak ada kesempatan jadi ada yang aku mau utarain dari sekarang...')
+                alert('ntah kamu udah nyadar atau belum karena emang biasanya feeling wanita itu kan oper power banget yahh malah bisa kita kira kaya dukun bisa tau gituhh wkwk')
+                alert('Tapi kalo toh kamu udah tau aku bakal tetep utarain, dan kalo belum aku bakal utarain secara jelas disini yahh')
+                alert('Jadi...')
+                alert('Akutuh dari dulu mungkin tepatnya dari semester 1 tuh sebetulnya ada perasaan sama kamu,  cuman karena memang purpose aku untuk kuliah itu buat upgrade skill dan ambil gelar jadi dulu sebelumnya aku jujur kurang peduli ke hal hal yang terkait sama hubungan dan bahkan bisa dibilang punya janji kediri sendiri untuk "Aku kuliah untuk belajar dan untuk lulus buat dapetin kerjaan yang lebih layak dari sebelumnya dan bisa mengangkat derajat keluarga..."')
+                alert('Dan bahkan kalo kamu masih inget waktu kita ngobrol pas PKKMB yang ngobrolin pengalaman organisasi, ngajar, dan kamu yang ikutan organisasi GMD. Aku tuh disana ngerasa biasa aja even anak anak yang didepan sempet nyorakin kita kann dan setelah disorakin itu kita langsung nggk ngobrol apa apa wkwk...')
+                alert('Singkat cerita, setelah dari PKKMB itu aku nggk ada kepikiran sama sekali soal kamu karena yahh mungkin masih dengan prinsip aku yang " yahh yaudahlah kita fokus kuliah gituu kann wkwk."')
+                alert('Tapi seiring berjalannya waktu banyak orang yang ngebahas sama ceritain kamu, awalnyna sih aku nggk yang terlalu nanggepin yahh tapi lama kelamaan aku juga secara nggk sadar jadi ikut merhatiinn hahah, rill lingkungan sangat mempengaruhi wkwk.')
+                alert('Dan mulai dari sana aku mulai merhatiin kamu, yang mungkin aku pikir mulai dari sana deh aku ada persaan sama kamu. I don\'t know why but thats my feels')
+                alert('Berjalan mungkin udah 3 tahun semenjak persaannya muncul, aku nggk pernah sama sekali kepikiran untuk langsung bilang ataupun jelasin perasaan ini sama kamu karena memang ada beberapa pertimbangan yang why aku lakuin itu.')
+                alert('Dan sebelum semuanya selesai ada satu hal yang ingin aku lakukan sebelum kelak aku sesali karena tidak ada keberanian untuk mengungkapkan.')
+                alert('I know maybe this little weird because You was have a boyfriend, but I\'ll do this for my self because I don\'t want make my future me dissapointed')
+                alert('With this message I\'ll tell about how I fall in love with you, and I still save this feels almost three years since our first meet. Tbh, I scared to explain this, I scared to know what the answer. But, maybe if you cannot answer now i\'ts okay I\'ll always wait. If someday You have problem and you haven\'t someone to hear your problem just call me and I\'ll be there.')
+                alert('Dan mungkin itu semua yang aku mau ungkapin, sekarang aku nggk berharap jawaban apapun. Karena aku yang sekarang tidak berniat untuk menembak tapi hanya mengungkapkan perasaan. Tapi, disini aku akan mulai berproses untuk menyiapkan segala hal untuk aku agar bisa siap menyatakan "I wanna Marry You"')
+                alert('Dan yah mungkin ini jadi hal yang cukup awkward yah dihidup kamu tp yahh begitulah aku juga nggk nyangka bisa begini. Thanks yah... dan kalo butuh sesuatu kaya temen cerita, keluh kesah atau apapun itu yang bisa kubantu tinggal infoin aja yahh.')
+                alert('Dan mungkin yang terakhir kalo kamu nggk keberatan, aku mau tau pov kamu terhadap aku tuh kayak gimana, mungkin itu bisa jadi referensi buat aku agar bisa berkembang...')
+                alert('Thank Youu' + ' ! ' + 'And see You on the Top !.')
+
+                break
+            
+            default :
+                alert('Kayaknya nama lengkapnya salah deh')
+
+        }
+
+        break
+
+    default :
+        alert('Alah siah punten nyasar, ini bukan buat kamu hhe.')
+        break
+}
